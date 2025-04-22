f4f258d5816dc0cf06a01dc29c5fdd33baf192fb
HondamirKobilov <118079181+HondamirKobilov@users.noreply.github.com>
2025-04-22 22:02:32 +0500

    3

--- eduWebApp/settings.py
+++ eduWebApp/settings.py
@@ -56,9 +56,11 @@ WSGI_APPLICATION = 'eduWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 
