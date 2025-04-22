55864c0f3d3a2204b20f550f9963c2d0dc053402
HondamirKobilov <118079181+HondamirKobilov@users.noreply.github.com>
2025-04-23 00:21:45 +0500

    6223erf

--- eduWebApp/settings.py
+++ eduWebApp/settings.py
@@ -104,7 +104,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-[]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
